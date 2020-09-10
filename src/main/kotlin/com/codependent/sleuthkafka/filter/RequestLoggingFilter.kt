7ce7eb61bef8cdab49795fecadbfe68016fc7670
jainigo <jainigo@profile.es>
2020-09-10 09:42:03 +0200

    PI-411 improved request filter

--- src/main/kotlin/com/codependent/sleuthkafka/filter/RequestLoggingFilter.kt
+++ src/main/kotlin/com/codependent/sleuthkafka/filter/RequestLoggingFilter.kt
@@ -13,21 +13,21 @@ class RequestLoggingFilter : WebFilter {
     private val logger = LoggerFactory.getLogger(javaClass)
 
     override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
+        val method = exchange.request.method
         val uri = exchange.request.uri
+        val headers = exchange.request.headers
         if (!uri.path.contains("/actuator")) {
-            if (!uri.path.contains("/actuator")) {
-                if (logger.isTraceEnabled) {
-                    val headers = exchange.request.headers
-                    logger.trace("|---> Request - URI {} - headers {}", uri, headers)
-                } else if (logger.isDebugEnabled) {
-                    logger.debug("|---> Request - URI {}", uri)
-                }
+            if (logger.isTraceEnabled) {
+
+                logger.trace("|---> Request - {} {} - headers {}", method, uri, headers)
+            } else if (logger.isDebugEnabled) {
+                logger.debug("|---> Request - {} {}", method, uri)
             }
         }
         return chain.filter(exchange)
                 .doOnTerminate {
                     if (!uri.path.contains("/actuator")) {
-                        logger.debug("|---> Response - URI {} - statusCode {}", uri, exchange.response.statusCode)
+                        logger.debug("|---> Response - {} {} - statusCode {}", method, uri, exchange.response.statusCode)
                     }
                 }
     }
